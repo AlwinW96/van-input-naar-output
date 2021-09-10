818a472a3e119a4114a52a9ffc027452fcacd9ef
alwinW96 <99060433@mydavinci.nl>
2021-09-10 09:43:44 +0200

    2de commit

---
+++ feestlunch.py
@@ -0,0 +1,2 @@
+bedrag = 18.88
+print('de feestlunch kost je bij de bakker'' ' + str( bedrag) +' ' 'euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
---
+++ speeldaghal.py
@@ -0,0 +1,2 @@
+bedrag = int(44.44)
+print('dit geweldige dagje-uit met 4 mensen in de speelhal met 45minuten VR kost je maar'' ' +str(bedrag) + ' ''euro')
